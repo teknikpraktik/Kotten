07186d0517f0d4e9a943328250361e36fcadc60e
teknikpraktik <per.a.bjorkman@gmail.com>
2026-07-15 22:06:46 +0200

    Refresh Kotten pastel app icons

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 const themeColor = '#fbf4e8';
 const backgroundColor = '#fbf4e8';
-const cacheId = 'kotten-clean-flow-v2';
+const cacheId = 'kotten-pastel-icon-v3';
 
 export default defineConfig({
   plugins: [
